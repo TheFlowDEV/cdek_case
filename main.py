6b2b7f1a4c70233a6df7a82cea3c0edcd4042ab5
FlowKal <wladimir12032005@gmail.com>
2024-09-11 12:47:55 +0700

    fix some bugs(and add datasets)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import keras
 import cv2
 from keras.applications import VGG16
-from keras.layers import Concatenate,Sequential,Dense,Dropout,Flatten
+from keras.layers import Sequential,Dense,Dropout,Flatten
+from keras.layers.merge import concatenate
+
 from keras.regularizers import l2
+from datasets import load_dataset
+ds = load_dataset("EmbeddingStudio/amazon-products-with-images")
 
 img_width=224
 img_height=224
@@ -19,7 +23,7 @@ def make_final_model(CNN_model,vec_size,n_classes,dropout=0.5,l2_strength=1e-5):
     image_model.add(Dense(256,activation="relu",W_regularizer=l2(l2_strength)))
     CNN_model.add(image_model)# добавляем к исходной CNN модели
 
-    merged = Concatenate([CNN_model,text_model])
+    merged = concatenate([CNN_model,text_model])
     final_model = Sequential()
     final_model.add(merged)
     final_model.add(Dropout(dropout))
