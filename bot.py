c1e8bebcad7920d02c07b7f872b54b40d527cddd
Глеб Михайлов <global-elite8@yandex.ru>
2024-09-12 12:18:45 +0700

    new botik

--- bot.py
+++ bot.py
@@ -1,46 +1,51 @@
-import telebot
-from io import BytesIO
-import numpy as np
-from telebot import types
-import translater as tr
-import emoji # для оформления
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import filters
+from aiogram.utils import executor
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
-info_text = f'Бот был разработан командой Kodiki{emoji.emojize(':cat:')} на Хакатоне-ТПУ 11.09.2024'
+# Токен вашего бота
 TELEGRAM_API_TOKEN = '7532048730:AAGDuCvqvWcsGP2pddWqZSc4NqM96vY4Ncw'
-bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
+bot = Bot(token=TELEGRAM_API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+info_text = 'Бот был разработан командой Kodiki на Хакатоне-ТПУ 11.09.2024'
+
 
 # Вступление, две кнопки: инфо + анализ
-@bot.message_handler(commands=['start'])
-def start_bot(message):
-    if message.text=='/start':
-        markup = types.InlineKeyboardMarkup()
-        # Кнопка анализа товара
-        button_promt_analyse = types.InlineKeyboardButton(f'Проанализировать товар{emoji.emojize(':label:')}', callback_data = 'analyse_of_data')
-        # Кнопка информации о создателях
-        button_info = types.InlineKeyboardButton(f'Информация о боте{emoji.emojie(':robot:')}', callback_data = 'info')
-        markup.add(button_promt_analyse)
-        markup.add(button_info)
-    else:
-        bot.send_message(message.chat.id,f'Запустите бота командой /start')
+@dp.message_handler(commands=['start'])
+async def start_bot(message: types.Message):
+    await message.answer("Привет ✌️")
+    markup = types.InlineKeyboardMarkup()
+    button_promt_analyse = types.InlineKeyboardButton('Проанализировать товар', callback_data='analyse_of_data')
+    button_info = types.InlineKeyboardButton('Информация о боте', callback_data='info')
+    markup.add(button_promt_analyse, button_info)
+    await message.answer("Выберите опцию:", reply_markup=markup)
 
-# Получение заппроса(данных о товаре)
-@bot.message_handler(commands=['text'])
-def get_data(message):
-    bot.send_message(message.chat.id, text = 'Пришлите описание, фотографию товара: ')
-    promt_desc = tr.client_promt(message.text) # Описание товара с переводом на en
-    promt_view = message.photo                 # Фотография товара
-    return [promt_desc, promt_view]
+
+# Получение запроса (данных о товаре)
+@dp.message_handler(commands=['text'])
+async def get_data(message: types.Message):
+    await message.answer('Пришлите описание, фотографии товара: ')
+
 
 # Вывод сообщений
-@bot.callback_query_handler(func= lambda call: True)
-def callback_worker(call):
+@dp.callback_query_handler(lambda call: True)
+async def callback_worker(call: types.CallbackQuery):
     if call.data == 'analyse_of_data':
-        bot.delete_message(chat_id = call.message.chat.id, message_id = call.message.id)
-        bot.send_message(call.message.chat.id, f'сработал')
+        await call.answer()  # Удаляем уведомление о нажатии кнопки
+        await call.message.delete()  # Удаляем предыдущее сообщение
+        await call.message.answer('сработал')  # Здесь можно добавить логику анализа товара
     elif call.data == 'info':
-        bot.delete_message(chat_id = call.message.chat.id, message_id = call.message.id)
-        bot.send_message(call.message.chat.id, info_text)        
+        await call.answer()  # Удаляем уведомление о нажатии кнопки
+        await call.message.delete()  # Удаляем предыдущее сообщение
+        await call.message.answer(info_text)
 
-# if __name__ == "__main__":
-#     main()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
